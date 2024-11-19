15b1f7e9d603e394c875be12b7ff17267d969112
RicoGunawan12 <ricogunawan12323@gmail.com>
2024-11-19 11:55:25 +0700

    test push

--- integration/komship.integration.js
+++ integration/komship.integration.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 
 const myHeaders = new Headers();
 myHeaders.append("x-api-key", process.env.KOMSHIP_API);
@@ -39,15 +40,52 @@ export const calculateDeliveryFeeKomship = async (shipperDestinationId, receiver
     
     try {
         const komshipResponse = await fetch(`https://api.collaborator.komerce.my.id/tariff/api/v1/calculate?shipper_destination_id=${shipperDestinationId}&receiver_destination_id=${receiverDestinationId}&weight=${weight}&item_value=${itemValue}&cod=${cod}`, requestOptions);
-        
         if (!komshipResponse.ok) {
-            throw new Error("Failed to calculate delivery fee");
+            throw new Error("Failed to calculate delivery fee: " + komshipResponse.statusText);
         }
-
+    
         const calculation = await komshipResponse.json();
         return calculation
     } catch (error) {
         throw new Error(error.message);
     }
     
+}
+
+// "order_date": "2024-05-29 23:59:59",
+// "brand_name": "Komship",
+// "shipper_name": "Toko Official Komship",
+// "shipper_phone": "6281234567689",
+// "shipper_destination_id": 17588,
+// "shipper_address": "order address detail",
+// "shipper_email":"test@gmail.com",
+// "receiver_name": "Buyer A",
+// "receiver_phone": "6281209876543",
+// "receiver_destination_id": 17589, 
+// "receiver_address": "order destination address detail",
+// "shipping": "JNT",
+// "shipping_type": "EZ",
+// "payment_method": "COD",
+// "shipping_cost":22000,
+// "shipping_cashback":10000,
+// "service_fee":2500,
+// "additional_cost":1000,
+// "grand_total":317000,
+// "cod_value":317000,
+// "insurance_value": 1000,
+// "order_details": [
+//     {
+//         "product_name": "Komship package",
+//         "product_variant_name": "Komship variant product",
+//         "product_price": 500000,
+//         "product_width": 5,
+//         "product_height": 2,
+//         "product_weight": 5100,
+//         "product_length": 20,
+//         "qty": 1,
+//         "subtotal": 500000
+//     }
+// ]
+export const createOrderKomship = async () => {
+
 }
