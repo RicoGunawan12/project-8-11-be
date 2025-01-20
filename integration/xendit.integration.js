be0e44c36900c45c5aa9d5b94b0dc7c3281e6ca1
Rico <ricogunawan12323@gmail.com>
2025-01-20 22:33:50 +0700

    update: fix bad request in xendit plan

--- integration/xendit.integration.js
+++ integration/xendit.integration.js
@@ -193,7 +193,7 @@ export const createPlanXendit = async (transaction, customerId, productsInCart, 
         return {
             type: "PHYSICAL_PRODUCT",
             name: product.product_variant.product.productName + " - " + product.product_variant.productColor,
-            net_unit_amount: product.product_variant.productPrice,
+            net_unit_amount: product.product_variant.productPrice === 0 ? 1 : product.product_variant.productPrice,
             quantity: product.quantity,
             url: process.env.BASE_URL + product.product_variant.productImage
             // url: "https://th.bing.com/th/id/OIP.ULq5QQnJfNFuhcLNBVqzAwHaE7?w=250&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
