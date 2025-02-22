0b50d178700f36f8e13a3ddc89aac940aa361efd
Vionny <senvionny@gmail.com>
2025-02-22 19:51:35 +0700

    -

--- services/metadata.service.js
+++ services/metadata.service.js
@@ -2,12 +2,14 @@ import { MetadataModel } from "../association/association.js";
 
 export const getMetadataService = async (slug) => {
 
-  const metadata = await MetadataModel.findOne({
+  var metadata = await MetadataModel.findOne({
     where: { slug: slug },
   });
 
   if (!metadata) {
-    throw new Error("Metadata not found for slug:", slug);
+    metadata = await MetadataModel.findOne({
+      where: { slug: 'default' },
+    });
   }
 
   return {
